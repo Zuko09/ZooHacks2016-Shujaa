0367a88aa6048bc4044fcc011603401fedbc4940
Aaron Harris <contactaaronh@gmail.com>
2016-10-09 12:52:39 -0700

    Changes names to be more compact

--- js/app.js
+++ js/app.js
@@ -1,36 +1,33 @@
-(function() {
+$(function() {
     'use strict';
 
-    angular.module("shujaaApp", [])
-    .controller("shujaaController", shujaaController);
-
-    shujaaController.$inject = ['$scope', '$filter'];
-    function shujaaController($scope, $filter) {
         // ELEMENTS:
         //  Start View
-        $scope.loadStartView = function() {
-            $scope.showingStartView = true;
-            $scope.showingMainView = false;
-            $scope.showingChatlogView = false;
-            $scope.showingMinimapView = false;
-        };
-        $scope.hideStartView = function() {
-            $scope.loadMainView();
-            $scope.loadChatlogView();
-            $scope.loadMinimapView();
-            $scope.showingStartView = false;
-        };
-        //  Main View
-        $scope.loadMainView = function() {
-            $scope.showingMainView = true;
-        };
-        //  CHAT LOG View
-        $scope.loadChatlogView = function() {
-            $scope.showingChatlogView = true;
-        };
-        //  Minimap View
-        $scope.loadMinimapView = function() {
-            $scope.showingMinimapView = true;
-        };
+    var loadStartView = function() {
+        $scope.showingStartView = true;
+        $scope.showingMainView = false;
+        $scope.showingChatlogView = false;
+        $scope.showingMinimapView = false;
+    var hideStartView = function() {
+        $scope.loadMainView();
+        $scope.loadChatlogView();
+        $scope.loadMinimapView();
+        $scope.showingStartView = false;
+    };
+
+    //  Main View
+    var loadMainView = function() {
+        $scope.showingMainView = true;
+    };
+
+    //  CHAT LOG View
+    var loadChatlogView = function() {
+        $scope.showingChatlogView = true;
+    };
+
+    //  Minimap View
+    var loadMinimapView = function() {
+        $scope.showingMinimapView = true;
+    };
     }
-})();
+});
